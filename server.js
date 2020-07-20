61aa622da50dedef9e9651e11c5470e6b1dbe585
Paul Stevenson <pablo@Pauls-MacBook-Pro.local>
2020-07-20 21:20:18 +0100

    db auth changed and db connection completed

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
