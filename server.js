34d79d5fd9a96a2438b3bf71a070c144a0ece107
Paul Stevenson <pablo@Pauls-MacBook-Pro.local>
2020-07-22 13:31:20 +0100

    added basic validation for user using express-validator

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 //Connect to Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
